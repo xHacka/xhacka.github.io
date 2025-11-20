d58dbea2ea61cbefda8c474ddc7c0fe2adad015a
xHacka <pvpgamerpro457@gmail.com>
2025-11-20 23:14:25 +0400

    Update package.json (final(2? fuck ganalytics i guess))

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,14 +2,8 @@ import DefaultTheme from "vitepress/theme";
 import Layout from "./Layout.vue";
 import "./custom_containers.css";
 import "./custom_title.css";
-import googleAnalytics from "vitepress-plugin-google-analytics";
 
 export default {
-    ...DefaultTheme,
-    ...Layout,
-    enhanceApp: () => {
-        googleAnalytics({
-            id: "G-6XT8WV8JH8",
-        });
-    },
+  extends: DefaultTheme,
+  Layout,
 };
